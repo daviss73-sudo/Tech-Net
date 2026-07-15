28ba5964f078430794c817efd6b24de99ec5428d
daviss73-sudo <daviss73@matc.edu>
2026-07-15 15:53:06 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import basicSsl from '@vitejs/plugin-basic-ssl';
 
 export default defineConfig({
   plugins: [react(), basicSsl()],
-  base: '/tech-net/',
+  base: '/Tech-Net/',
   server: {
     port: 3000,
     open: true,

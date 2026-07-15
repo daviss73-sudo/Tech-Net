c77de1f9005ee200f8142c5fb289745d7ef494f6
daviss73-sudo <daviss73@matc.edu>
2026-07-15 10:00:24 -0500

    Add files via upload

---
+++ vite.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import basicSsl from '@vitejs/plugin-basic-ssl';
+
+export default defineConfig({
+  plugins: [react(), basicSsl()],
+  base: '/tech-net/',
+  server: {
+    port: 3000,
+    open: true,
+  },
+});
